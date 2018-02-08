802c750b4aca8a0459d7e7bd3dd7d8d66efa3168
Felipe Netto <flpncc@gmail.com>
2018-02-08 12:52:47 -0300

    Telling client to expect JSON  and explicitly setting the status code

--- handlers.go
+++ handlers.go
@@ -17,6 +17,9 @@ func TodoIndexHandler(w http.ResponseWriter, r *http.Request) {
 		Todo{Name: "My presentation"},
 		Todo{Name: "Host meetup"},
 	}
+	
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
